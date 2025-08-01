9e316bb2ff0e18388c0aa2cd946a99dae44f3bf8
Mireli-afk <gvasquezd22_2@unc.edu.pe>
2025-08-01 17:34:25 -0500

    V8.0
    Incluimos nuevas interfaces con botones y una nueva pestaña

--- js/script.js
+++ js/script.js
@@ -2,14 +2,26 @@
 // ¡Esto es un ejemplo! Debes llenarlo con los datos reales de tus lotes.
 const datosLotes = {
     // Manzana A
-    "A-1": 120, "A-2": 125, "A-3": 130,
+    "A-20": 120, "A-21": 125, "A-22": 130, "A-23": 130,
     // Manzana B
-    "B-1": 110, "B-2": 115, "B-3": 118,
+    "B-19": 110, "B-32": 115, "B-34": 118,
+    // Manzana E
+    "E-20": 150, "E-21": 155, "E-14": 160, "E-24": 160, "E-43": 160,
+    // Manzana F
+    "F-1": 150, "F-11": 155, "F-12": 160, "F-13": 160, "F-14": 160,
+    "F-15": 150, "F-16": 155, "F-17": 160, "F-18": 160, "F-19": 160,
+    "F-21": 160, "F-25": 160, "F-26": 160,
+    // Manzana G
+    "G-1": 150, "G-11": 155, "G-12": 160,
+    // Manzana G
+    "H-11": 150, "H-12": 155, "H-14": 160, "H-21": 160,
+    // Manzana I
+    "I-5": 150, "I-6": 155, "I-7": 160, "I-12": 160,
     // Manzana J
-    "J-1": 150, "J-2": 155, "J-3": 160,
+    "J-19": 150, "J-20": 155, "J-21": 160, "J-22": 160,
 };
 
-const PRECIO_POR_METRO = 547; // El precio que me indicaste
+const PRECIO_POR_METRO = 547; 
 
 // --- Obtenemos los elementos del HTML con los que vamos a trabajar ---
 const inputManzana = document.getElementById('manzana');
@@ -56,3 +68,17 @@ function calcularPrecio() {
 // Hacemos que la función se ejecute cada vez que el usuario escribe en los campos
 inputManzana.addEventListener('input', calcularPrecio);
 inputLote.addEventListener('input', calcularPrecio);
+
+// --- LÓGICA PARA CAMBIAR VISTAS DE FINANCIAMIENTO ---
+const btnCalcularCuotas = document.getElementById('btn-calcular-cuotas');
+const vistaOpcionesPago = document.getElementById('vista-opciones-pago');
+const vistaFinanciamientoLote = document.getElementById('vista-financiamiento-lote');
+
+// Añadimos un "event listener" al botón
+btnCalcularCuotas.addEventListener('click', function() {
+    // Ocultamos la primera vista
+    vistaOpcionesPago.classList.add('hidden');
+    
+    // Mostramos la segunda vista
+    vistaFinanciamientoLote.classList.remove('hidden');
+});
